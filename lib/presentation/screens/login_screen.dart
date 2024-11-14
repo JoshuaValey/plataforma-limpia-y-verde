7dd74bc57e87d9b65bf62f5507992e9967867404
sebastianMLMJ <milianmejiaedgarsebastian@yahoo.com>
2024-11-13 22:46:53 -0600

    login

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:plataforma_limpia_y_verde/appi_service.dart';
+import 'package:plataforma_limpia_y_verde/presentation/Repository/usuario.dart';
 import 'package:plataforma_limpia_y_verde/presentation/widgets/green_button.dart';
 import 'package:plataforma_limpia_y_verde/singleton.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // Controladores para los campos de texto
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Limpiar los controladores cuando el widget se destruya
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    AppiService apiService = AppiService(url: Singleton.linkApiService);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Plataforma Limpia y Verde'),
+        leading: IconButton(
+          icon: const Icon(Icons.logout),
+          color: Colors.white,
+          onPressed: () {
+            // Acción al presionar el icono de logout
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -20,15 +48,16 @@ class LoginScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   const Image(
-                    image: AssetImage('assets/Logomuni1.png'),
+                    image: AssetImage('assets/lyv.png'),
                     height: 300,
                     width: 300,
                   ),
                   const SizedBox(height: 50),
                   ConstrainedBox(
                     constraints: const BoxConstraints(maxWidth: 400),
-                    child: const TextField(
-                      decoration: InputDecoration(
+                    child: TextField(
+                      controller: _usernameController, // Asignar controlador
+                      decoration: const InputDecoration(
                         labelText: 'Usuario',
                         border: OutlineInputBorder(),
                       ),
@@ -37,8 +66,9 @@ class LoginScreen extends StatelessWidget {
                   const SizedBox(height: 20),
                   ConstrainedBox(
                     constraints: const BoxConstraints(maxWidth: 400),
-                    child: const TextField(
-                      decoration: InputDecoration(
+                    child: TextField(
+                      controller: _passwordController, // Asignar controlador
+                      decoration: const InputDecoration(
                         labelText: 'Contraseña',
                         border: OutlineInputBorder(),
                       ),
@@ -47,11 +77,40 @@ class LoginScreen extends StatelessWidget {
                   ),
                   const SizedBox(height: 20),
                   GreenButton(
-                      label: 'Iniciar Sesión',
-                      onPressed: () {
-                        Singleton.instance.showToast('Bienvendio');
-                        Navigator.pushNamed(context, '/main_screen');
-                      })
+                    label: 'Iniciar Sesión',
+                    onPressed: () {
+                      // Obtener los valores de los campos de texto
+                     
+                      String usuario  =  _usernameController.text;
+                      String contrasenia = _passwordController.text;
+
+                      if (usuario.isEmpty || contrasenia.isEmpty) {
+                        // Validación simple
+                        //Singleton.showToast('Por favor ingrese usuario y contraseña');
+                        return;
+                      }
+
+                      Usuario usuarioLogin = Usuario(
+                      id: "",
+                      nombreUsuario: usuario,
+                      contrasenia: contrasenia,
+                      rol: "",
+                      );
+
+                      // Llamar a la API o hacer alguna acción con los datos
+                      apiService.login(usuarioLogin, '/usuario/iniciarsesion').then((response) {
+                        if (response.rol == "Monitor") {
+                          //Singleton.showToast('Bienvenido');
+                          Singleton.idUsuario = response.id;
+                          Navigator.pushNamed(context, '/main_screen');
+                        } else {
+                          //Singleton.showToast('Credenciales incorrectas');
+                        }
+                      }).catchError((e) {
+                        //Singleton.showToast('Error al intentar iniciar sesión');
+                      });
+                    },
+                  ),
                 ],
               ),
             ),
@@ -61,3 +120,4 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
